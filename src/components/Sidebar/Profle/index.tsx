f4f17550152884e8a5bae2d01f701f5dedaf768f
ejinn1 <ejinn1@naver.com>
2024-12-23 07:47:52 +0000

    Chore: add image url

--- src/components/Sidebar/Profle/index.tsx
+++ src/components/Sidebar/Profle/index.tsx
@@ -1,18 +1,56 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import Image from 'next/image';
+
+import { Skeleton } from '@/components/common/Skeleton';
 import { useUserQuery } from '@/hooks/apis/useUserQuery';
 
 export const Profile = () => {
-  const { email, name } = useUserQuery();
+  const { email, name, profile, isLoading } = useUserQuery();
+  const [showSkeleton, setShowSkeleton] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      const timer = setTimeout(() => setShowSkeleton(true), 200);
+      return () => clearTimeout(timer);
+    }
+
+    setShowSkeleton(false);
+  }, [isLoading]);
 
   return (
-    <div className="flex w-full gap-8 p-16">
-      <div className="size-37 shrink-0 rounded-8 bg-slate-200" />
-      <div className="flex w-full justify-between">
-        <div>
-          <p className="text-sm-medium">{name}</p>
-          <p className="text-xs-medium">{email}</p>
-        </div>
-        <button className="text-xs-normal">로그아웃</button>
-      </div>
+    <div className="flex min-h-53 w-full gap-8 p-16">
+      {showSkeleton ? (
+        <>
+          <Skeleton className="size-37 rounded-8" />
+          <div className="flex flex-col gap-8 py-4">
+            <Skeleton className="h-12 w-30 rounded-12" />
+            <Skeleton className="h-10 w-70 rounded-12" />
+          </div>
+        </>
+      ) : (
+        <>
+          {!isLoading && (
+            <Image
+              src={profile}
+              alt="profile picture"
+              width={37}
+              height={37}
+              className="shrink-0 rounded-8"
+              priority
+            />
+          )}
+          <div className="flex w-full justify-between">
+            <div>
+              <p className="text-sm-medium">{name}</p>
+              <p className="text-xs-medium">{email}</p>
+            </div>
+            <button className="text-xs-normal">로그아웃</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
