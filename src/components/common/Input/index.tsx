3afe9f30f934fefabca06a0e0fda3bd65a846f8f
ejinn1 <ejinn1@naver.com>
2025-05-17 16:37:39 +0900

    feat: inputField로 form input 관리

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,18 +1,18 @@
-import { forwardRef } from 'react';
+import { forwardRef } from "react";
 
-import { cn } from '@/utils/className';
+import { cn } from "@/utils/className";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type = 'text', ...props }, ref) => {
+  ({ className, type = "text", ...props }, ref) => {
     const inputClass = cn(
-      'w-full px-24 py-12 outline-none bg-white rounded-12 text-sm-normal md:text-base-normal',
-      className,
+      "w-full px-24 py-12 outline-none bg-white rounded-12 text-sm-normal md:text-base-normal",
+      className
     );
 
     return <input ref={ref} type={type} className={inputClass} {...props} />;
-  },
+  }
 );
 
-Input.displayName = 'Input';
+Input.displayName = "Input";
